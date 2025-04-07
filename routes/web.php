79c2bf3a818566c7c61b76ee4f207908a3f2959a
Jean Roa <stebanrc21@gmail.com>
2025-04-06 22:30:38 -0500

    IgnoreMigrations, Routes

--- routes/web.php
+++ routes/web.php
@@ -1,26 +1,38 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Auth\ConductorCodeController;
+use App\Http\Controllers\DriverController;
+use App\Http\Controllers\PassengerController;
 use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+Route::middleware(['auth', 'role:1'])->group(function () {
+    Route::resource('conductores', DriverController::class);
+    Route::get('/driver/{id}/edit', [DriverController::class, 'edit'])->name('driver.edit');
+    Route::put('/driver/{id}', [DriverController::class, 'update'])->name('driver.update');
+});
+
+Route::middleware(['auth', 'role:2'])->group(function () {
+    Route::resource('pasajeros', PassengerController::class);
+    Route::delete('/pasajeros/{solipasaje}', [PassengerController::class, 'destroy'])->name('pasajeros.destroy');
+});
+
+Route::get('/conductor/code', [ConductorCodeController::class, 'create'])->name('conductor.code');
+Route::post('/conductor/code', [ConductorCodeController::class, 'store'])->name('conductor.code.verify');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -28,4 +40,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
